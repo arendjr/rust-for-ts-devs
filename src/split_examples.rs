02c9d66d09ce1ba1691014ab3f519dd62316ec27
Arend van Beelen jr <arendjr@gmail.com>
2022-05-18 13:29:11 +0200

    Fix #1: Pass actual numbers to parse

--- src/split_examples.rs
+++ src/split_examples.rs
@@ -9,12 +9,12 @@ pub fn split_vec<'a>(string: &'a str, delim: &'_ str) -> Vec<&'a str> {
     string.split(delim).collect()
 }
 
-fn print_parsed_numbers() -> Result<(), std::num::ParseIntError> {
+pub fn print_parsed_numbers() -> Result<(), std::num::ParseIntError> {
     // 'static is a special lifetime that lives for the entire
     // duration of the program's execution:
     let words: &'static str = "one two three";
     let split_words = split_vec(words, " ");
-    let numbers = split(words, " ")
+    let numbers = split("1 2 3", " ")
         .map(|word| word.parse::<usize>())
         .collect::<Result<Vec<_>, _>>()?;
     for (word, number) in split_words.iter().zip(numbers.iter()) {
